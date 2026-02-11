d932f58a6fb5b6c819ad343cf6f51fe9da532a2d
jin <jinsu.kim1011@gmail.com>
2026-02-11 14:01:39 +0100

    Button: Add inline prop style

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -4,7 +4,7 @@ import type React from "react";
 import {
   BgClass,
   BorderClass,
-  TextClass,
+  TextColorClass,
   PaddingXClass,
   PaddingYClass,
   TextSizeClass,
@@ -14,18 +14,20 @@ import {
   ActiveBgClass,
   ActiveTextClass,
   ActiveBorderClass,
+  MarginXClass,
+  MarginYClass,
 } from "./types/tokens";
 
 type ButtonVariant = "fill" | "outlined" | "void";
 type ButtonSize = "mini" | "small" | "regular" | "large";
 
 const variantClasses: Record<ButtonVariant, {
-  base: `${BgClass} ${TextClass} ${BorderClass}`;
+  base: `${BgClass} ${TextColorClass} ${BorderClass}`;
   hover: `${HoverBgClass} ${HoverTextClass} ${HoverBorderClass}`;
   active: `${ActiveBgClass} ${ActiveTextClass} ${ActiveBorderClass}`;
 }> = {
   fill: {
-    base: "bg-surface-primary text-content-primary border-transparent",
+    base: "bg-surface-quaternary text-content-primary border-transparent",
     hover: "hover:bg-surface-secondary hover:text-content-primary hover:border-transparent",
     active: "active:bg-surface-tertiary active:text-content-secondary active:border-transparent"
   },
@@ -41,22 +43,44 @@ const variantClasses: Record<ButtonVariant, {
   }
 }
 
-const sizeClasses: Record<ButtonSize, `${TextSizeClass} ${PaddingXClass} ${PaddingYClass}`> = {
-  mini: "text-mini px-micro py-0",
-  small: "text-small px-microPlus py-micro",
-  regular: "text-small px-mini py-microPlus",
-  large: "text-regular px-mini py-mini"
+const sizeClasses: Record<ButtonSize, {
+  base: `${TextSizeClass} ${PaddingXClass} ${PaddingYClass}`,
+  hover: `-${MarginXClass} -${MarginYClass}`,
+  active: `-${MarginXClass} -${MarginYClass}`,
+}> = {
+  mini: {
+    base: "text-mini px-micro py-0",
+    hover: "-mx-micro -my-0",
+    active: "-mx-micro -my-0",
+  },
+  small: {
+    base: "text-small px-microPlus py-0",
+    hover: "-mx-microPlus -my-micro",
+    active: "-mx-microPlus -my-micro",
+  },
+  regular: {
+    base: "text-small px-mini py-microPlus",
+    hover: "-mx-mini -my-microPlus",
+    active: "-mx-mini -my-microPlus",
+  },
+  large: {
+    base: "text-regular px-mini py-mini",
+    hover: "-mx-mini -my-mini",
+    active: "-mx-mini -my-mini",
+  }
 }
 
 export interface ButtonProps extends Omit<React.ComponentPropsWithoutRef<"button">, "className"> {
   variant?: ButtonVariant;
   size?: ButtonSize;
+  inline?: boolean;
   fullWidth?: boolean;
 }
 
 export const Button = ({
   variant = "fill",
   size = "regular",
+  inline = false,
   fullWidth = false,
   children,
   ...props
@@ -66,15 +90,17 @@ export const Button = ({
     variantClasses[variant].base,
     variantClasses[variant].hover,
     variantClasses[variant].active,
-    sizeClasses[size],
+    sizeClasses[size].base,
+    inline ? sizeClasses[size].hover : "",
+    inline ? sizeClasses[size].active : "",
     fullWidth ? "w-full" : "w-auto",
     "border",
     "inline-flex items-center justify-center",
     "transition-colors transition-transform",
     "duration-quickTransition",
     "ease-outExpo",
-    "hover:underline hover:underline-offset-[4px]",
-    "active:translate-y-0.5 active:scale-[0.98]"
+    // "hover:underline hover:underline-offset-[4px]",
+    "active:translate-y-0.5"
   ]
     .filter(Boolean)
     .join(" ");
