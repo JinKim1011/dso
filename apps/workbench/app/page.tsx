4e353bed0c50ef0ebd54d14e498d8a21949c858a
jin <jinsu.kim1011@gmail.com>
2026-02-11 13:58:05 +0100

    Applied temporary layout for testing button variants style

--- apps/workbench/app/page.tsx
+++ apps/workbench/app/page.tsx
@@ -1,23 +1,32 @@
 import { Button } from "@repo/ui/button";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-
-    <div className={styles.page}>
-      <Button variant="fill" size="large" >fill-large</Button>
-      <Button variant="fill" size="regular" >fill-regular</Button>
-      <Button variant="fill" size="small" >fill-small</Button>
-      <Button variant="fill" size="mini" >fill-mini</Button>
-      <Button variant="outlined" size="large" >outlined-large</Button>
-      <Button variant="outlined" size="regular" >outlined-regular</Button>
-      <Button variant="outlined" size="small" >outlined-small</Button>
-      <Button variant="outlined" size="mini" >outlined-mini</Button>
-      <Button variant="void" size="large" >void-large</Button>
-      <Button variant="void" size="regular" >void-regular</Button>
-      <Button variant="void" size="small" >void-small</Button>
-      <Button variant="void" size="mini" >void-mini</Button>
-
+    <div className="flex flex-col gap-small">
+      <div className="flex gap-min">
+        <Button variant="fill" size="large" >fill-large</Button>
+        <Button variant="fill" size="regular" >fill-regular</Button>
+        <Button variant="fill" size="small" >fill-small</Button>
+        <Button variant="fill" size="mini" >fill-mini</Button>
+      </div>
+      <div className="flex gap-mini">
+        <Button variant="outlined" size="large" >outlined-large</Button>
+        <Button variant="outlined" size="regular" >outlined-regular</Button>
+        <Button variant="outlined" size="small" >outlined-small</Button>
+        <Button variant="outlined" size="mini" >outlined-mini</Button>
+      </div>
+      <div className="flex gap-mini">
+        <Button variant="void" size="large" >void-large</Button>
+        <Button variant="void" size="regular" >void-regular</Button>
+        <Button variant="void" size="small" >void-small</Button>
+        <Button variant="void" size="mini" >void-mini</Button>
+      </div>
+      <div className="flex gap-mini">
+        <Button variant="void" size="large" inline>void-large-inline</Button>
+        <Button variant="void" size="regular" inline>void-regular-inline</Button>
+        <Button variant="void" size="small" inline>void-small-inline</Button>
+        <Button variant="void" size="mini" inline>void-mini-inline</Button>
+      </div>
     </div>
   );
 }
