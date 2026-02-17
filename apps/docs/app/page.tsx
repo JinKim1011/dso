f411c137af68e62d58a0a492675e4f8e831b2a5a
jin <jinsu.kim1011@gmail.com>
2026-02-18 00:18:22 +0100

    wip(page.tsx): introduce theme toggle button to docs page for debugging

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { useTheme, Button } from "@repo/ui";
+
 export default function Home() {
+  const { theme, toggleTheme } = useTheme();
+
   return (
     <div className="">
+      <Button variant="outlined" size="regular" onClick={toggleTheme}>
+        {theme === "light" ? "Dark" : "Light"} Mode
+      </Button>
       <main className="">
-        <h1>Getting Started</h1>
+        <h2>Getting Started</h2>
         <p>
           DSO(Design System Oracle) is a visual Git client for design tokens. It
           lets designer audit components, ajdust token value and mappings, and
           generate clean commits that keep code aligned with the design system.
         </p>
-        <h2>Installation</h2>
+        <h3>Installation</h3>
       </main>
     </div>
   );
