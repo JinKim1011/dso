ec820bc705564dddf803058d54ae1b04a2d87089
jin <jinsu.kim1011@gmail.com>
2026-02-18 17:53:29 +0100

    refactor(token.ts): remove color related types

--- packages/ui/src/types/tokens.ts
+++ packages/ui/src/types/tokens.ts
@@ -1,17 +1,6 @@
 export * from "./typography";
 export * from "./motion";
-
-export type SurfaceVariant =
-  | "primary"
-  | "secondary"
-  | "tertiary"
-  | "quaternary";
-export type ContentVariant =
-  | "primary"
-  | "secondary"
-  | "tertiary"
-  | "quaternary";
-export type StrokeVariant = "primary" | "secondary" | "tertiary";
+export * from "./color";
 
 export type SpacingStep =
   | "micro"
@@ -24,32 +13,6 @@ export type SpacingStep =
   | "regularPlus"
   | "large"
   | "largePlus";
-
-export type BgClass = `bg-surface-${SurfaceVariant}` | "bg-transparent";
-export type TextColorClass = `text-content-${ContentVariant}` | "text-primary";
-export type BorderClass =
-  | `border-stroke-${StrokeVariant}`
-  | "border-transparent";
-
-export type HoverBgClass =
-  | `hover:bg-surface-${SurfaceVariant}`
-  | "hover:bg-transparent";
-export type HoverTextColorClass =
-  | `hover:text-content-${ContentVariant}`
-  | "hover:text-primary";
-export type HoverBorderClass =
-  | `hover:border-stroke-${StrokeVariant}`
-  | "hover:border-transparent";
-
-export type ActiveBgClass =
-  | `active:bg-surface-${SurfaceVariant}`
-  | "active:bg-transparent";
-export type ActiveTextColorClass =
-  | `active:text-content-${ContentVariant}`
-  | "active:text-primary";
-export type ActiveBorderClass =
-  | `active:border-stroke-${StrokeVariant}`
-  | "active:border-transparent";
 
 export type PaddingClass = `p-${SpacingStep}` | "p-0";
 export type PaddingXClass = `px-${SpacingStep}` | "px-0";
