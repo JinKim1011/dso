a941f1e9665a554a90c79c3bd4d28750bf4dbf90
jin <jinsu.kim1011@gmail.com>
2026-03-04 16:16:05 +0100

    fix(index.ts): correct casing for component imports

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,6 @@
 // Components
-export { Button } from "./button";
-export { Text } from "./text";
+export { Button } from "./Button";
+export { Text } from "./Text";
 
 // Hooks
 export { useTheme } from "./hooks/useTheme";
