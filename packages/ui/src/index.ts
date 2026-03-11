83744dbebabf050bda731581c90adb12ab50e6ad
jin <jinsu.kim1011@gmail.com>
2026-03-11 08:56:28 +0100

    Export SegmentedControl component from index

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,7 @@
 // Components
 export { Button } from "./Button";
 export { Text } from "./Text";
+export { SegmentedControl } from "./SegmentedControl";
 
 // Hooks
 export { useTheme } from "./hooks/useTheme";
