688155fea4acad07da9d768ce0ec0c93445588ec
jin <jinsu.kim1011@gmail.com>
2026-03-11 10:03:47 +0100

    wip(workbench): test segmented control interaction

--- apps/workbench/app/page.tsx
+++ apps/workbench/app/page.tsx
@@ -1,8 +1,30 @@
+"use client";
+
 import { Button } from "@repo/ui";
+import { SegmentedControl } from "@repo/ui";
+import { SunIcon, MoonIcon, LaptopIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
+
+const themeOptions = [
+  { value: "light", iconName: SunIcon },
+  { value: "dark", iconName: MoonIcon },
+  { value: "system", iconName: LaptopIcon },
+];
 
 export default function Home() {
+  const [theme, setTheme] = useState("light");
+
   return (
     <div className="gap-small flex flex-col">
+      <SegmentedControl
+        options={themeOptions}
+        size="sm"
+        iconOnly={true}
+        onChange={setTheme}
+        value={theme}
+      />
+      <Button variant="void" iconOnly={true} size="md" leftIcon={SunIcon} />
+
       <div className="gap-mini flex items-center">
         <Button variant="fill" size="lg">
           fill/lg
