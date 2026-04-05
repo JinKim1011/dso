7e4e861dc2a1e31c816dcf7f38c60b8623a44dc1
jin <jinsu.kim1011@gmail.com>
2026-04-05 21:49:56 +0200

    feat(Listbox): define Listbox component that map the ListboxOption

---
+++ packages/ui/src/components/combobox/Listbox.tsx
@@ -0,0 +1,45 @@
+import { type IconComponent, ListboxOption } from "./ListboxOption";
+
+type ListboxItem = {
+  value: string;
+  label: string;
+  helperText?: string;
+  rightIcon?: IconComponent;
+  disabled?: boolean;
+};
+
+type ListboxProps = {
+  options: ListboxItem[];
+  selectedValue: string;
+  onSelect: (value: string) => void;
+};
+
+export function Listbox({ options, selectedValue, onSelect }: ListboxProps) {
+  return (
+    <div className="py-microPlus rounded-mini shadow-overlay-menu flex flex-col">
+      {options.map((option) => {
+        const isDisabled = Boolean(option.disabled);
+        const isSelected = option.value === selectedValue;
+        const handleSelect = () => {
+          if (isDisabled) {
+            return;
+          }
+
+          onSelect(option.value);
+        };
+
+        return (
+          <ListboxOption
+            key={option.value}
+            label={option.label}
+            helperText={option.helperText}
+            rightIcon={option.rightIcon}
+            selected={isSelected}
+            disabled={isDisabled}
+            onSelect={handleSelect}
+          ></ListboxOption>
+        );
+      })}
+    </div>
+  );
+}
