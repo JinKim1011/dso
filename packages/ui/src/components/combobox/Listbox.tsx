2ac7615511637b569cb5b6336cadaf2d32e0b983
jin <jinsu.kim1011@gmail.com>
2026-04-07 07:54:39 +0200

    chore(Listbox): change Listbox posion to absoulte with translate y

--- packages/ui/src/components/combobox/Listbox.tsx
+++ packages/ui/src/components/combobox/Listbox.tsx
@@ -16,15 +16,12 @@ type ListboxProps = {
 
 export function Listbox({ options, selectedValue, onSelect }: ListboxProps) {
   return (
-    <div className="py-microPlus rounded-mini shadow-overlay-menu flex flex-col">
+    <div className="py-microPlus px-micro rounded-mini shadow-overlay-menu absolute flex w-full translate-y-[0.25rem] flex-col">
       {options.map((option) => {
         const isDisabled = Boolean(option.disabled);
         const isSelected = option.value === selectedValue;
         const handleSelect = () => {
-          if (isDisabled) {
-            return;
-          }
-
+          if (isDisabled) return;
           onSelect(option.value);
         };
 
