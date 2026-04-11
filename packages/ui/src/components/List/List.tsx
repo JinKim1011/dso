fb55b095127b3496af51827b9f8461b9ebba98b6
jin <jinsu.kim1011@gmail.com>
2026-04-11 20:27:38 +0200

    feat(List.tsx): create List component by mapping ListItem component with framer motion applied

---
+++ packages/ui/src/components/List/List.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { motion, type HTMLMotionProps } from "framer-motion";
+import { createMotionTransition } from "../../types/motion";
+import { ListItem, type ListItemProps } from "./ListItem";
+
+export interface ListProps extends Omit<HTMLMotionProps<"ul">, "className"> {
+  listItems: ListItemProps[];
+}
+
+const listTransition = createMotionTransition("quick", "outExpo");
+
+const listVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: { ...listTransition, staggerChildren: 0.02 },
+  },
+};
+
+export const List = ({ listItems, ...props }: ListProps) => {
+  return (
+    <motion.ul
+      {...props}
+      variants={listVariants}
+      initial="hidden"
+      animate="visible"
+      className="px-miniPlus gap-microPlus flex flex-col"
+    >
+      {listItems.map((item, itemIndex) => (
+        <ListItem key={item.index ?? item.text ?? itemIndex} {...item} />
+      ))}
+    </motion.ul>
+  );
+};
