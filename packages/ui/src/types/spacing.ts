0416599a06f6597b2d5d0c72b7f3bdb3ecc82923
jin <jinsu.kim1011@gmail.com>
2026-04-22 00:30:19 +0200

    feat(spacing): add padding class uniton types for top, right, bottom, and left

--- packages/ui/src/types/spacing.ts
+++ packages/ui/src/types/spacing.ts
@@ -13,6 +13,10 @@ export type SpacingStep =
 export type PaddingClass = `p-${SpacingStep}` | "p-0";
 export type PaddingXClass = `px-${SpacingStep}` | "px-0";
 export type PaddingYClass = `py-${SpacingStep}` | "py-0";
+export type PaddingTClass = `pt-${SpacingStep}` | "pt-0";
+export type PaddingRClass = `pr-${SpacingStep}` | "pr-0";
+export type PaddingBClass = `pb-${SpacingStep}` | "pb-0";
+export type PaddingLClass = `pl-${SpacingStep}` | "pl-0";
 
 export type MarginClass = `m-${SpacingStep}` | "m-0";
 export type MarginXClass = `mx-${SpacingStep}` | "mx-0";
