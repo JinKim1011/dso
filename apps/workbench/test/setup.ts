fe5be72dffaff0095240783c5b2759fdd845fb1f
jin <jinsu.kim1011@gmail.com>
2026-04-28 12:58:02 +0200

    feat(setup): define ResizeObserver to let React Flow mount without crashing in jsdom

--- apps/workbench/test/setup.ts
+++ apps/workbench/test/setup.ts
@@ -1,1 +1,12 @@
 import "@testing-library/jest-dom";
+
+class ResizeObserver {
+  observer() {}
+  unobserver() {}
+  disconnect() {}
+}
+
+Object.defineProperty(globalThis, "ResizeObserver", {
+  value: ResizeObserver,
+  writable: true,
+});
