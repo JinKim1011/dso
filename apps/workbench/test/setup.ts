a131cba9db06478eee3cdb17fe01eb71a0802b70
jin <jinsu.kim1011@gmail.com>
2026-04-28 13:00:39 +0200

    feat(setup): define getBoundingClientRect to give React Flow a stable size (re: fitview, node positioning)

--- apps/workbench/test/setup.ts
+++ apps/workbench/test/setup.ts
@@ -10,3 +10,17 @@ Object.defineProperty(globalThis, "ResizeObserver", {
   value: ResizeObserver,
   writable: true,
 });
+
+Object.defineProperty(HTMLElement.prototype, "getBoundingClientRect", {
+  value: () => ({
+    x: 0,
+    y: 0,
+    top: 0,
+    left: 0,
+    right: 1024,
+    bottom: 768,
+    width: 1024,
+    height: 768,
+    toJSON: () => {},
+  }),
+});
