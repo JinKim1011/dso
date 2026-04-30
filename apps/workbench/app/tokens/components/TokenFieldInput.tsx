2a3d9ab8e9e5fa4904ef103980832ea7231f2bf9
jin <jinsu.kim1011@gmail.com>
2026-04-30 18:17:16 +0200

    feat(TokenFieldInput): add new component for token input field

---
+++ apps/workbench/app/tokens/components/TokenFieldInput.tsx
@@ -0,0 +1,40 @@
+import { Input, Text } from "@repo/ui";
+
+type TokenFieldInputProps = {
+  id: string;
+  label: string;
+  value: string;
+  onChange: (value: string) => void;
+  placeholder?: string;
+};
+
+export function TokenFieldInput({
+  id,
+  label,
+  value,
+  onChange,
+  placeholder,
+}: TokenFieldInputProps) {
+  return (
+    <div className="gap-mini grid grid-cols-3">
+      <div className="col-span-1 my-auto">
+        <Text
+          as="label"
+          htmlFor={id}
+          variant="input-label-sm"
+          className="text-content-primary"
+        >
+          {label}
+        </Text>
+      </div>
+      <div className="col-span-2">
+        <Input
+          id={id}
+          value={value}
+          placeholder={placeholder}
+          onChange={(event) => onChange(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
