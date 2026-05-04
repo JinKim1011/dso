86180dfee19171359952e526a71edf75fd3c5001
jin <jinsu.kim1011@gmail.com>
2026-05-04 15:58:12 +0200

    chore: replace WorkbenchShellProvider with NavigationSlotProvider in WorkbenchShell component

--- apps/workbench/app/_shared/shell/WorkbenchShell.tsx
+++ apps/workbench/app/_shared/shell/WorkbenchShell.tsx
@@ -1,8 +1,8 @@
 import { type ReactNode } from "react";
 import designTokensManifest from "../../../../../design-tokens-manifest.json";
 import { buildTokenGraphModel } from "../../tokens/lib/manifestAdapter";
+import NavigationSlotProvider from "../context/NavigationSlotContext";
 import { StagedManifestProvider } from "../context/StagedManifestContext";
-import WorkbenchShellProvider from "../context/WorkbenchShellContext";
 import { Header } from "./Header";
 import { Navigation } from "./Navigation";
 
@@ -18,7 +18,7 @@ export function WorkbenchShell({ children }: WorkbenchShellProps) {
   const userName = process.env.NEXT_PUBLIC_WORKBENCH_USER_NAME ?? "jin1011";
 
   return (
-    <WorkbenchShellProvider>
+    <NavigationSlotProvider>
       <StagedManifestProvider baseManifest={result.model}>
         <div>
           <Header currentBranch={currentBranch} userName={userName}></Header>
@@ -26,6 +26,6 @@ export function WorkbenchShell({ children }: WorkbenchShellProps) {
           <main>{children}</main>
         </div>
       </StagedManifestProvider>
-    </WorkbenchShellProvider>
+    </NavigationSlotProvider>
   );
 }
