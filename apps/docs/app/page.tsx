5f0e2191c73940a74c4a0b928bc5c27652ff6017
jin <jinsu.kim1011@gmail.com>
2026-05-04 03:18:52 +0200

    chore: reorder imports and remove unused content from Home component

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useTheme, Button } from "@repo/ui";
+import { Button, useTheme } from "@repo/ui";
 
 export default function Home() {
   const { theme, toggleTheme } = useTheme();
@@ -10,15 +10,7 @@ export default function Home() {
       <Button variant="fill" size="lg" onClick={toggleTheme}>
         {theme === "light" ? "Dark" : "Light"} Mode
       </Button>
-      <main className="">
-        <h2>Getting Started</h2>
-        <p>
-          DSO(Design System Oracle) is a visual Git client for design tokens. It
-          lets designer audit components, ajdust token value and mappings, and
-          generate clean commits that keep code aligned with the design system.
-        </p>
-        <h3>Installation</h3>
-      </main>
+      <main className=""></main>
     </div>
   );
 }
