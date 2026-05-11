ebe1c30744b12adcbbbc3caf0bde3131ae3a2e9c
jin <jinsu.kim1011@gmail.com>
2026-05-11 12:04:35 +0200

    feat(StagedRowDetail.tsx): add StagedRowDetail component for displaying before and after values with visual indicators

---
+++ apps/workbench/app/staged/component/StagedRowDetail.tsx
@@ -0,0 +1,69 @@
+import { RowSpacingIcon } from "@radix-ui/react-icons";
+import { TokenTypeValueItem } from "../../tokens/lib/manifestAdapter";
+import { StagedRowDetailTag } from "./StagedRowDetailTag";
+import { StagedRowDetailValue } from "./StagedRowDetailValue";
+
+type StagedRowDetailProps = {
+  id: string;
+  beforeName: string;
+  afterName: string;
+  before: TokenTypeValueItem;
+  after: TokenTypeValueItem;
+};
+
+export function StagedRowDetail({
+  id,
+  beforeName,
+  afterName,
+  before,
+  after,
+}: StagedRowDetailProps) {
+  const wrapperStyle = [
+    "w-full flex flex-col h-full",
+    "py-largePlus px-large gap-regular",
+    "bg-dot-pattern overflow-hidden",
+  ].join(" ");
+
+  return (
+    <aside className={wrapperStyle} data-testid={`detail: ${id}`}>
+      <div className="gap-regular flex w-full flex-col">
+        <div className="item-start flex flex-col" data-testid={`after-detail: ${id}`}>
+          <StagedRowDetailTag
+            label="AFTER"
+            background="bg-surface-primary"
+            textColor="text-content-accentStrong"
+          />
+          <div className="py-miniPlus bg-surface-primary gap-micro flex h-fit w-full flex-col">
+            <StagedRowDetailValue
+              type="afterName"
+              beforeName={beforeName}
+              afterName={afterName}
+            />
+            <StagedRowDetailValue type="afterValue" after={after} before={before} />
+          </div>
+        </div>
+        <StagedRowDetailTag
+          align="justify-center"
+          label="Changes"
+          textColor="text-content-accentStrong"
+          leftIcon={RowSpacingIcon}
+        />
+        <div className="item-start flex flex-col" data-testid={`before-detail: ${id}`}>
+          <StagedRowDetailTag
+            label="BEFORE"
+            background="bg-surface-primary"
+            textColor="text-content-accentStrong"
+          />
+          <div className="py-miniPlus bg-surface-primary gap-mirco flex h-fit w-full flex-col">
+            <StagedRowDetailValue
+              type="beforeName"
+              beforeName={beforeName}
+              afterName={afterName}
+            />
+            <StagedRowDetailValue type="beforeValue" before={before} after={after} />
+          </div>
+        </div>
+      </div>
+    </aside>
+  );
+}
