64874b274d132f27c53ba1c4712346ac80f96208
jin <jinsu.kim1011@gmail.com>
2026-06-16 11:55:33 +0200

    style(Navigation): remove cursor-grab class from navigation drag container

--- apps/workbench/app/_shared/shell/Navigation.tsx
+++ apps/workbench/app/_shared/shell/Navigation.tsx
@@ -38,7 +38,7 @@ export function Navigation() {
         right: 500,
         bottom: 0,
       }}
-      className="gap-microPlus fixed bottom-10 left-1/2 z-10 flex -translate-x-1/2 cursor-grab items-end"
+      className="gap-microPlus fixed bottom-10 left-1/2 z-10 flex -translate-x-1/2 items-end"
     >
       <nav aria-label="navigation" className={navWrapperStyles}>
         {navigationSlot && (
