e56cee6f58e11da440a8e98fa1ae83c15ec7f421
JinKim1011 <jinsu.kim1011@gmail.com>
2026-06-30 12:45:37 +0200

    fix(TokenColorForm): update import path for TokenFieldInput component

--- apps/workbench/app/tokens/color/components/TokenColorForm.tsx
+++ apps/workbench/app/tokens/color/components/TokenColorForm.tsx
@@ -1,4 +1,4 @@
-import { TokenFieldInput } from "../../_shared/components/TokenFieldInput";
+import { TokenFieldInput } from "../../components/TokenFieldInput";
 
 export type TokenColorDraft = {
   name: string;
